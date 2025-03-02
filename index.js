2952d3430d4ffa18a9ae72139f0d24a491ee3ae2
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-03-02 11:18:05 +0300

    Array methods

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Write your solution here!
+
+const cats = ["Milo","Otis","Garfield"]
+
+function  destructivelyAppendCat(name) {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop('Garfield');
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift('Milo');
+}
+
+function appendCat(name) {
+    return [...cats, 'Broom'];
+}
+
+function prependCat(name) {
+    return [ 'Arnold' , ...cats];
+}
+
+function  removeLastCat() {
+    // return  [...cats, 'Broom'];
+    return cats.slice(0, -1)
+}
+
+function  removeFirstCat(){
+    console.log(`${cats}`)
+    return cats.slice(1)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
